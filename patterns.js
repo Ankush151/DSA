de6c9281c6b9529e29c8e2404f651b84ea73d7d2
Ankush151 <ankushanand60@gmail.com>
2026-07-04 23:33:40 +0530

    Refactor reverseTriangleStars function and add revedrseTriangleNumbers function

--- patterns.js
+++ patterns.js
@@ -79,7 +79,25 @@ function reverseTriangleStars(n){
         for(let j=0; j<n-i+1; j++){
             row+="* "
         }
-        console.log(row)
+        // console.log(row)
     }
 }
 reverseTriangleStars(5)
+
+
+// 6.   1 2 3 4 5
+//      1 2 3 4
+//      1 2 3
+//      1 2
+//      1
+
+function revedrseTriangleNumbers(n){
+    for(let i = 0; i <= n; i++){
+        let row = ""
+        for(let j = 1; j<n-i+1; j++){
+            row+= `${j} `
+        }
+        // console.log(row)
+    }
+}
+revedrseTriangleNumbers(5)
