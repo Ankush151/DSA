aed691badbc5ee75b3c0b1276f00e073df99ea0d
Ankush151 <ankushanand60@gmail.com>
2026-07-04 23:09:02 +0530

    numberTriangle2

--- patterns.js
+++ patterns.js
@@ -46,7 +46,24 @@ function numberTriangle(n){
         for(let  j = 1; j <= i; j++){
             row+= `${j} `
         }
-        console.log(row)
+        // console.log(row)
     }
 }
 numberTriangle(5)
+
+// 4.   1 
+//      2 2
+//      3 3 3  
+//      4 4 4 4 
+//      5 5 5 5 5 
+
+function numberTriangle2(n){
+    for(let i = 1; i<=n; i++){
+        let row = ""
+        for(let j = 1; j <= i; j++){
+            row+= `${i} `
+        }
+        console.log(row)
+    }
+}
+numberTriangle2(5)
