811b392aae1234d36d8dff5eed5757dd1b2dd914
Ankush151 <ankushanand60@gmail.com>
2026-07-04 19:01:29 +0530

    numberTriangle pattern

--- patterns.js
+++ patterns.js
@@ -28,8 +28,25 @@ function printTriangle(n){
         for(let j = 0; j <= i; j++){
             row+= "* "
         }
-        console.log(row)
+        // console.log(row)
     }
 }
 printTriangle(5)
 
+
+// 3.   1 
+//      1 2 
+//      1 2 3 
+//      1 2 3 4 
+//      1 2 3 4 5 
+
+function numberTriangle(n){
+    for(let i = 1; i<=n; i++){
+        let row = ""
+        for(let  j = 1; j <= i; j++){
+            row+= `${j} `
+        }
+        console.log(row)
+    }
+}
+numberTriangle(5)
