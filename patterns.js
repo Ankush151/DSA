513104eb7551a99cb2da418c867258bcdf4a96fd
Ankush151 <ankushanand60@gmail.com>
2026-07-04 13:09:14 +0530

    print printTriangle logic

--- patterns.js
+++ patterns.js
@@ -1,17 +1,35 @@
 // Write logic to print patterns 
 
-// 1.   ****
-//      ****
-//      ****
-//      ****
+// 1.   * * * *
+//      * * * *
+//      * * * *
+//      * * * *
 
 function printSquarePattern(n) {
     for(let i = 0; i < n; i++) {
         let row = '';
         for(let j = 0; j < n; j++) {
-            row+= "*";
+            row+= "* ";
         }
-        console.log(row);
+        // console.log(row);
     }
 }
 await printSquarePattern(4);
+
+// 2.   *
+//      * *
+//      * * *
+//      * * * *
+//      * * * * *
+
+function printTriangle(n){
+    for(let i = 0; i< n; i++){
+        let row = ""
+        for(let j = 0; j <= i; j++){
+            row+= "* "
+        }
+        console.log(row)
+    }
+}
+printTriangle(5)
+
