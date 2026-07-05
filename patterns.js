d35e8165dc817e17baf06a5646fcad4b1ff9750c
Ankush151 <ankushanand60@gmail.com>
2026-07-05 19:53:00 +0530

    Add reversePyramidStars function to print a reverse pyramid pattern

--- patterns.js
+++ patterns.js
@@ -125,7 +125,29 @@ function pyramidStars(n){
         //     row+= " "
         // }
 
-        console.log(row)
+        // console.log(row)
     }
 }
 pyramidStars(5)
+
+
+function reversePyramidStars(n){
+    for(let i = 0; i< n; i++){
+        let row = ""
+        //for space
+        for(let j = 0; j < i; j++){
+            row += " "
+        }
+
+        //for stars
+        for(let j = 0; j < 2*n - (2*i +1); j++){
+            row += "* "
+        }
+
+        for(let j = 0; j < i; j++){
+            row += " "
+        }    
+        console.log(row)
+    }
+}
+reversePyramidStars(5)
